7c9e867d77e4263a33c019353e25df35b8e795c8
Igor <62031924+SpaceHolderdJs@users.noreply.github.com>
2023-03-14 00:50:02 +0200

    Added * for CORS policy

--- src/config/cors-options.ts
+++ src/config/cors-options.ts
@@ -18,10 +18,11 @@ export const corsOptions: boolean | CorsOptions | CorsOptionsDelegate<any> = (
 ) => {
   let corsOptions: CorsOptions;
 
-  if (corsAllowedUrls.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { credentials: true, origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { credentials: true, origin: false }; // disable CORS for this request
-  }
+//   if (corsAllowedUrls.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { credentials: true, origin: true }; // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { credentials: true, origin: false }; // disable CORS for this request
+//   }
+  corsOptions = { credentials: true, origin: true };
   callback(null, corsOptions);
 };
